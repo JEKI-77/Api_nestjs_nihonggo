f73b232d802b640e1dc79e688eee73a8b13a0e2d
Jeki Saputra <saputrajeki90@gmail.com>
2023-09-01 04:33:59 +0700

    validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import 'dotenv/config';
 // import { APP_FILTER } from '@nestjs/core';
 // import { HttpErrorFilter } from './shared/http_error.filter';
 import { KosakataModule } from './kosakata/kosakata.module';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './user/users.module';
 import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,14 +2,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { forwardRef, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { UsersModule } from '../users/users.module';
+import { UsersModule } from '../user/users.module';
 import { AuthService } from './auth.service';
 import { jwtConstants } from '../constants/constants';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 import { AuthController } from './auth.controller';
 import { RolesGuard } from './guards/roles.guard';
-import { User } from '../users/entity/users.entity';
+import { User } from '../user/entity/users.entity';
 
 @Module({
   imports: [
