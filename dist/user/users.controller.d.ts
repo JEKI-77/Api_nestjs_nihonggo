9a8ebc3554b4e3bd5b441cc79af9c252d77594df
Jeki Saputra <saputrajeki90@gmail.com>
2023-09-02 01:33:02 +0700

    fix/bug

--- dist/user/users.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { CreateUserDto } from './dto/create-user.dto';
-import { UsersService } from './users.service';
-export declare class UsersController {
-    private readonly usersService;
-    constructor(usersService: UsersService);
-    create(createUserDto: CreateUserDto): Promise<import("./entity/users.entity").User>;
-    getProfile(req: any): any;
-}
