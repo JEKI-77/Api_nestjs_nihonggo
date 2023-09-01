366b1732282471b1cb1b6501bbb26380f2deedcf
Jeki Saputra <saputrajeki90@gmail.com>
2023-09-01 21:15:53 +0700

    fix/bug

--- dist/kosakata/dto/kosakata.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.kosakataDTO = void 0;
-class kosakataDTO {
-}
-exports.kosakataDTO = kosakataDTO;
-//# sourceMappingURL=kosakata.dto.js.map
